1199d9a1a0ef5dff8d64fbd22127bf5439e16f79
Alexey Solovyov <me@alsol85.ru>
2023-03-25 07:54:13 +0300

    add silero_tts

---
+++ silero_tts.py
@@ -0,0 +1,54 @@
+import os
+import torch
+from pydub import AudioSegment
+
+device = torch.device('cpu')
+torch.set_num_threads(4)
+local_file = 'v3_1_ru.pt'
+
+if not os.path.isfile(local_file):
+    torch.hub.download_url_to_file(
+        'https://models.silero.ai/models/tts/ru/v3_1_ru.pt', local_file)
+
+model = torch.package.PackageImporter(
+    local_file).load_pickle("tts_models", "model")
+model.to(device)
+
+
+INPUT_FILE = './txt/input.txt'
+INPUT_DIR = os.path.dirname(os.path.realpath(INPUT_FILE))
+INPUT_NAME = os.path.splitext(os.path.basename(INPUT_FILE))[0]
+
+
+with open(INPUT_FILE, mode='r+', encoding='utf-8') as txt_in:
+    example_text = txt_in.read()
+
+
+sample_rate = 48000
+speakers = [
+    'aidar',
+    'baya',
+    'eugene',
+    'kseniya',
+    'xenia',
+    # 'random',
+]
+
+for speaker in speakers:
+    audio_lst = []
+
+    for line in example_text.split('\n'):
+        if not line:
+            audio_lst.append(AudioSegment.silent(duration=300))
+            continue
+        print(line)
+        audio_paths = model.save_wav(text=line,
+                                     speaker=speaker,
+                                     sample_rate=sample_rate)
+        audio_lst.append(AudioSegment.from_file("test.wav", format="wav"))
+
+    combined = AudioSegment.empty()
+    for a in audio_lst:
+        combined += a
+
+    file_handle = combined.export(os.path.join(INPUT_DIR, f"{INPUT_NAME}_{speaker}.mp3"), format="mp3")
